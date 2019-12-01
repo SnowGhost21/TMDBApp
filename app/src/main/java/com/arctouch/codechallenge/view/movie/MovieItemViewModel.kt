afc71d539bfa6b214e9f87cd5bf695466a54d55f
diego.cunha <domdiegobc@gmail.com>
2019-12-01 12:58:16 -0300

    DEV :: Removed unused val

--- app/src/main/java/com/arctouch/codechallenge/view/movie/MovieItemViewModel.kt
+++ app/src/main/java/com/arctouch/codechallenge/view/movie/MovieItemViewModel.kt
@@ -1,6 +1,5 @@
 package com.arctouch.codechallenge.view.movie
 
-import android.text.TextUtils
 import com.arctouch.codechallenge.model.data.Movie
 
 class MovieItemViewModel(private val movie: Movie) {
@@ -8,8 +7,6 @@ class MovieItemViewModel(private val movie: Movie) {
     val imagePath = movie.posterPath
 
     val title = movie.title
-
-    val genresOverview: String = if (movie.genres != null) TextUtils.join(",", movie.genres) else "No genre defined"
 
     val releaseDate = movie.releaseDate
 
